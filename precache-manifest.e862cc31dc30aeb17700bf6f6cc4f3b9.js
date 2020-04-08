963c9a6f1c1fb7539c1052c191ea9bf8e31e2f6d
Yibo-Hao <sxyhao@hotmail.com>
2020-04-08 15:34:38 +0800

    deploy

---
+++ precache-manifest.e862cc31dc30aeb17700bf6f6cc4f3b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40dd064160776e9870b",
+    "url": "/bookkeeping-vueVersion-dist/css/app.85fd0044.css"
+  },
+  {
+    "revision": "28db5eeee5e51840729a7c74385a472a",
+    "url": "/bookkeeping-vueVersion-dist/index.html"
+  },
+  {
+    "revision": "c40dd064160776e9870b",
+    "url": "/bookkeeping-vueVersion-dist/js/app.a30261a1.js"
+  },
+  {
+    "revision": "e628b7f4cd500a4f4cb7",
+    "url": "/bookkeeping-vueVersion-dist/js/chunk-vendors.b63330d2.js"
+  },
+  {
+    "revision": "600b3268dc34dfb79da5d33e5d7573b7",
+    "url": "/bookkeeping-vueVersion-dist/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/bookkeeping-vueVersion-dist/robots.txt"
+  }
+]);
